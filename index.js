9401d21a430c5fc0027939dddea28bd765f5a35f
nadim <mdnadimmahamud704@gmail.com>
2023-07-25 14:44:37 +0600

    first task done

---
+++ index.js
@@ -0,0 +1,13 @@
+/****
+ * Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+ */
+
+// const reverseString = (inputString) => {
+//   let reverseStr = '';
+//   for (let i = inputString.length - 1; i >= 0; i--) {
+//     reverseStr += inputString[i];
+//   }
+//   return reverseStr;
+// };
+// const inputString = 'Hello World';
+// console.log(reverseString(inputString));
