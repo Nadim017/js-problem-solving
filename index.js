cdf7048804de632ae90adb8bdef8f33bbfe62a7a
nadim <mdnadimmahamud704@gmail.com>
2023-07-25 15:40:47 +0600

    task4 done

--- index.js
+++ index.js
@@ -16,9 +16,37 @@
  * Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
  */
 
-const numbers = [2, -5, 10, -3, 7];
+// const numbers = [2, -5, 10, -3, 7];
 
-const positiveNumFunc = (numbers) => {
-  return numbers.reduce((sum, num) => (num > 0 ? sum + num : sum), 0);
+// const positiveNumFunc = (numbers) => {
+//   return numbers.reduce((sum, num) => (num > 0 ? sum + num : sum), 0);
+// };
+// console.log(positiveNumFunc(numbers));
+
+/**
+ * 
+Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
+
+ */
+const array = [1, 3, 6, 8, 11, 15];
+
+const findTwoNumbersWithSum = (array, targetValue) => {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    const sum = array[left] + array[right];
+
+    if (sum === targetValue) {
+      return [left, right];
+    } else if (sum < targetValue) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return null;
 };
-console.log(positiveNumFunc(numbers));
+
+console.log(findTwoNumbersWithSum(array, 9));
